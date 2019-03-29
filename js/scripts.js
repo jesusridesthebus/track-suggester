a028ca8cbc4356f12bb00013920ebb84470b3bc0
Liz <jesusridesthebus@gmail.com>
2019-03-29 16:01:51 -0700

    add function first if statement to logic in js

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,12 @@ $(function() {
     $('.result-css-design').hide();
 
     var frontBack = $('input:radio[name=second-question]:checked').val();
-    var curriculum = $('input:radio[name=fourth-question]:checked').val();
+    var curriculum = $('#curriculum-module').val();
     var company = $('input:radio[name=fifth-question]:checked').val();
 
-    if (frontBack === '')
+    if (frontBack === 'back2' && curriculum === 'php4' && company === 'php5') {
+      $('.result-php').show();
+    }
 
   });
 });
