d505dcb1d1b2e3ebff160c49df089bb56cf5aa3d
Liz <jesusridesthebus@gmail.com>
2019-03-29 16:38:11 -0700

    add js logic to select quiz answer, but right now it is only choosing based onthe last question.

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,33 @@ $(function() {
     var curriculum = $('#curriculum-module').val();
     var company = $('input:radio[name=fifth-question]:checked').val();
 
-    if (frontBack === 'back2' && curriculum === 'php4' && company === 'php5') {
+    if (frontBack === 'back2' || curriculum === 'php4' && company === 'php5') {
+      $('.result-php').show();
+    } else if (frontBack === 'back2' && curriculum === 'php4' || company === 'php5') {
+      $('.result-php').show();
+    } else if (frontBack === 'back2' || curriculum === 'c-sharp4' && company === 'c-sharp5') {
+      $('.result-c-sharp').show();
+    } else if (frontBack === 'back2' && curriculum === 'c-sharp4' || company === 'c-sharp5') {
+      $('.result-c-sharp').show();
+    } else if (frontBack === 'back2' || curriculum === 'ruby4' && company === 'ruby5') {
+      $('.result-ruby').show();
+    } else if (frontBack === 'back2' && curriculum === 'ruby4' || company === 'ruby5') {
+      $('.result-ruby').show();
+    } else if (curriculum === 'java4' || company === 'java5') {
+      $('.result-java').show();
+    } else if (frontBack === 'front2' || curriculum === 'css-design4' && company === 'css-design5') {
+      $('.result-css-design').show();
+    } else if (frontBack === 'front2' && curriculum === 'css-design4' || company === 'css-design5') {
+      $('.result-css-design').show();
+    } else if (curriculum === 'ruby4') {
+      $('.result-ruby').show();
+    } else if (curriculum === 'java4') {
+      $('.result-java').show();
+    } else if (curriculum === 'css-design4') {
+      $('.result-css-design').show();
+    } else if (curriculum === 'c-sharp4') {
+      $('.result-c-sharp').show();
+    } else if (curriculum === 'php4') {
       $('.result-php').show();
     }
 
