c6a9a16c689b2b4351f084f21bdcd87c28ad42ad
Liz <jesusridesthebus@gmail.com>
2019-03-31 14:30:11 -0700

    add minor edits to text content in html, fix logic in js with adding parentheses, which will fix previous problems

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-$(function() {
-  $('#track-form').submit(function(event) {
+$(function () {
+  $('#track-form').submit(function (event) {
     event.preventDefault();
     $('.result-c-sharp').hide();
     $('.result-java').hide();
@@ -8,26 +8,28 @@ $(function() {
     $('.result-css-design').hide();
 
     var frontBack = $('input:radio[name=second-question]:checked').val();
-    var curriculum = $('#curriculum-module').val();
+    var curriculum = $('#curriculum-module option:selected').val();
     var company = $('input:radio[name=fifth-question]:checked').val();
 
-    if (frontBack === 'back2' || curriculum === 'php4' && company === 'php5') {
+    console.log(curriculum);
+
+    if ((frontBack === 'back2' || curriculum === 'php4') && company === 'php5') {
       $('.result-php').show();
-    } else if (frontBack === 'back2' && curriculum === 'php4' || company === 'php5') {
+    } else if (frontBack === 'back2' && (curriculum === 'php4' || company === 'php5')) {
       $('.result-php').show();
-    } else if (frontBack === 'back2' || curriculum === 'c-sharp4' && company === 'c-sharp5') {
+    } else if ((frontBack === 'back2' || curriculum) === 'c-sharp4' && company === 'c-sharp5') {
       $('.result-c-sharp').show();
-    } else if (frontBack === 'back2' && curriculum === 'c-sharp4' || company === 'c-sharp5') {
+    } else if (frontBack === 'back2' && (curriculum === 'c-sharp4' || company === 'c-sharp5')) {
       $('.result-c-sharp').show();
-    } else if (frontBack === 'back2' || curriculum === 'ruby4' && company === 'ruby5') {
+    } else if ((frontBack === 'back2' || curriculum) === 'ruby4' && company === 'ruby5') {
       $('.result-ruby').show();
-    } else if (frontBack === 'back2' && curriculum === 'ruby4' || company === 'ruby5') {
+    } else if (frontBack === 'back2' && (curriculum === 'ruby4' || company === 'ruby5')) {
       $('.result-ruby').show();
     } else if (curriculum === 'java4' || company === 'java5') {
       $('.result-java').show();
-    } else if (frontBack === 'front2' || curriculum === 'css-design4' && company === 'css-design5') {
+    } else if ((frontBack === 'front2' || curriculum === 'css-design4') && company === 'css-design5') {
       $('.result-css-design').show();
-    } else if (frontBack === 'front2' && curriculum === 'css-design4' || company === 'css-design5') {
+    } else if (frontBack === 'front2' && (curriculum === 'css-design4' || company === 'css-design5')) {
       $('.result-css-design').show();
     } else if (curriculum === 'ruby4') {
       $('.result-ruby').show();
